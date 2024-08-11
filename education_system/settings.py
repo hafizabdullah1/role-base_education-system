c9fb048ef4f232c604d72b3bfe7550c773050978
Abdullah2723 <127143084+Abdullah2723@users.noreply.github.com>
2024-08-12 01:09:38 +0500

    setup vercel deployment

--- education_system/settings.py
+++ education_system/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-dp$3=ennrrg0u@gm!)--+=o@3(&endt5l0ovl2(s4sh0#qa*8!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 # Application definition
 
@@ -127,6 +127,8 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
